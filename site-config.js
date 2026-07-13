7d183da45f4c9217cb1e33411e9b1f7d6901bbf5
TLILI MOHAMMED LARBI <gareena.free.fire.1123@gmail.com>
2026-07-13 23:03:00 +0100

    Add files via upload

--- site-config.js
+++ site-config.js
@@ -1,5 +1,4 @@
 window.PORTFOLIO_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbyuTsRflR04d1KB8YDliCZTmXkiRA7Ny5YH4qpPr7U6qA-t_oviPC3MoXkwYkVBdI3P/exec",
-  adminUrl: "https://script.google.com/macros/s/AKfycbyuTsRflR04d1KB8YDliCZTmXkiRA7Ny5YH4qpPr7U6qA-t_oviPC3MoXkwYkVBdI3P/exec?page=admin",
-  version: "2.0.0"
+  apiUrl: "https://script.google.com/macros/s/AKfycbxPda3CvFPy63fMY6F3-2m6oFEkJb9AZGW53u6MRLXGF7aoJJhzpmW5mzTPtaMoXWfgPg/exec",
+  adminUrl: "https://script.google.com/macros/s/AKfycbxPda3CvFPy63fMY6F3-2m6oFEkJb9AZGW53u6MRLXGF7aoJJhzpmW5mzTPtaMoXWfgPg/exec?page=admin"
 };
